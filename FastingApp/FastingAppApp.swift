d6e09fdde01e008a85867fbb24a7c6419e566d48
Dmytro Golub <dmytro.golub@gmail.com>
2025-08-10 17:51:49 +0100

    Initial Commit

---
+++ FastingApp/FastingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingAppApp.swift
+//  FastingApp
+//
+//  Created by Dmytro Golub on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FastingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
